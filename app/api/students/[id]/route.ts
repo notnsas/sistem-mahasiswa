b57cae8d4e7e808110093f44f55a797d1c20f2df
notnsas <ramapanjinararendra@gmail.com>
2026-06-08 23:28:35 +0700

    benerin fix id string di api

--- app/api/students/[id]/route.ts
+++ app/api/students/[id]/route.ts
@@ -2,14 +2,14 @@ import { NextResponse } from "next/server"
 import { db } from "@/db"
 import { eq } from "drizzle-orm"
 import { students } from "@/db/schema"
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest } from 'next'
 
 export const GET = async (
   req: NextApiRequest,
   { params }: { params: Promise<{ id: number }> }
 ) => {
   console.log("GET request received with URL:", req.url)
-  const id = (await params).id
+  const id = Number((await params).id)
   console.log("Extracted id from query:", id)
   const currentStudent = await db.query.students.findFirst({
     where: eq(students.id, id),
